8dd318153f343bece96b25c21fac618d59a959d4
William Mosquera <wjmmk3@gmail.com>
2025-10-03 15:21:20 -0500

    feat CheckOrthography method added.

--- src/app/Presentation/Services/opeai.service.ts
+++ src/app/Presentation/Services/opeai.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
+import { orthographyUseCase } from "app/Core/use-cases/orthography/orthography-usecase";
+import { Message } from "app/Interfaces";
+import { from } from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class OpenAiService {
 
+  checkOrthography(prompt: string) {
+    return from(orthographyUseCase(prompt));
+  }
 }
