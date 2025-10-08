ca0c15b03e524a676a683c5b857fb0d99923c0f3
William Mosquera <wjmmk3@gmail.com>
2025-10-08 09:59:32 -0500

    feat: checkProsConsStream method created.

--- src/app/Presentation/Services/opeai.service.ts
+++ src/app/Presentation/Services/opeai.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { orthographyUseCase } from "app/Core/use-cases/orthography/orthography-usecase";
+import { prosConsStreamUseCase, prosConsStreamUseCaseGenerator } from "app/Core/use-cases/pros-cons/proscons-stream.usecase";
 import { orthographyUseCaseProsCons } from "app/Core/use-cases/pros-cons/proscons-usecase";
 import { from } from "rxjs";
 
@@ -12,5 +13,9 @@ export class OpenAiService {
 
   checkProsCons(prompt: string) {
     return from(orthographyUseCaseProsCons(prompt));
+  }
+
+  checkProsConsStream(prompt: string, abortSignal: AbortSignal) {
+    return prosConsStreamUseCaseGenerator(prompt, abortSignal); // Se dejó timpo promesa para manejar el stream en el componente de una forma más directa.
   }
 }
